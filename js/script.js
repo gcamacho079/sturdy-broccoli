522669ff315ec1ca9866376e798dd38a81775fc3
Lupe <lupe@acoorg.com>
2018-02-14 13:42:51 -0800

    Add price toggle functionality and smoosh down for mobile viewing

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
 var pricingTable = {
   init: function() {
-    $(".table-expander").click(pricingTable.toggleIt);
+    $(".table-expander").click(pricingTable.toggleTable);
+    $(".priceToggle").click(pricingTable.togglePrice);
   },
 
-  toggleIt: function() {
+  toggleTable: function() {
     if ($(".table-expander").hasClass("expander--closed")) {
       $(".row--hidden").toggle();
       $(".expander-arrow").text("▲");
-      $(this).removeClass("expander--closed").addClass("expander--open");
+      $(this).removeClass("expander--closed")
+             .addClass("expander--open");
     }
     else {
       $(".expander-arrow").text("▼");
       $(".row--hidden").toggle();
-      $(this).removeClass("expander--open").addClass("expander--closed");
+      $(this).removeClass("expander--open")
+             .addClass("expander--closed");
+    }
+  },
+
+  togglePrice: function() {
+    const monthlyBasic = "$147";
+    const monthlyPro = "$167";
+    const monthlyPreferred = "$247";
+
+    const annuallyBasic = "$117";
+    const annuallyPro = "$137";
+    const annuallyPreferred = "$197";
+
+    $(".priceToggle").removeClass("priceToggle--active");
+    $(this).addClass("priceToggle--active");
+    let nowShow = $(this).attr("data-this");
+    console.log(nowShow);
+    if (nowShow == "monthly") {
+      $("#basicPrice").html(monthlyBasic);
+      $("#proPrice").html(monthlyPro);
+      $("#preferredPrice").html(monthlyPreferred);
+    }
+    else {
+      $("#basicPrice").html(annuallyBasic);
+      $("#proPrice").html(annuallyPro);
+      $("#preferredPrice").html(annuallyPreferred);
     }
   }
 }
