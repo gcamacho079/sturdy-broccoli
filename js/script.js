4e0032077c4aa31cdde39911123ef38c4fe9aba9
Lupe <lupe@acoorg.com>
2018-02-14 12:26:20 -0800

    Add table expansion functionality and nonfunctional monthly/weekly toggle

--- js/script.js
+++ js/script.js
@@ -1,28 +1,20 @@
+var pricingTable = {
+  init: function() {
+    $(".table-expander").click(pricingTable.toggleIt);
+  },
 
-$(document).ready(function() {
-  var checkoutURL = "https://www.directoryforcontractors.org/checkout/";
-  var linkArray = {"basic1":"36", "basic2":"56", "basic3":"57", "basic4":"58",
-  "pro1":"38", "pro2":"59", "pro3":"60", "pro4":"61",
-  "preferred1":"40", "preferred2":"62", "preferred3":"63", "preferred4":"64"};
-
-  $('select[name="basic-select"]').change(function() {
-    if ($(this).val() !== "") {
-      var variable = "basic" + $(this).val();
-      $("#basic-select-button").attr("disabled", false).removeClass("select-join--disabled").addClass("select-join--enabled").attr("href", checkoutURL + linkArray[variable]);
+  toggleIt: function() {
+    if ($(".table-expander").hasClass("expander--closed")) {
+      $(".row--hidden").toggle();
+      $(".expander-arrow").text("▲");
+      $(this).removeClass("expander--closed").addClass("expander--open");
     }
-  });
-
-  $('select[name="pro-select"]').change(function() {
-    if ($(this).val() !== "") {
-      var variable = "pro" + $(this).val();
-      $("#pro-select-button").attr("disabled", false).removeClass("select-join--disabled").addClass("select-join--enabled").attr("href", checkoutURL + linkArray[variable]);
+    else {
+      $(".expander-arrow").text("▼");
+      $(".row--hidden").toggle();
+      $(this).removeClass("expander--open").addClass("expander--closed");
     }
-  });
+  }
+}
 
-  $('select[name="preferred-select"]').change(function() {
-    if ($(this).val() !== "") {
-      var variable = "preferred" + $(this).val();
-      $("#preferred-select-button").attr("disabled", false).removeClass("select-join--disabled").addClass("select-join--enabled").attr("href", checkoutURL + linkArray[variable]);
-    }
-  });
-});
+$(document).ready(pricingTable.init);
