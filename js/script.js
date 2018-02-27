9b34d8bb0a498c42aaffc5e90751c701d80068a8
Lupe <lupe@acoorg.com>
2018-02-27 12:32:32 -0800

    Redesign other tables based on design and remove unnecessary toggle scripts

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 var pricingTable = {
   init: function() {
     $(".table-expander").click(pricingTable.toggleTable);
-    $(".priceToggle").click(pricingTable.togglePrice);
   },
 
   toggleTable: function() {
@@ -16,35 +15,6 @@ var pricingTable = {
       $(".row--hidden").toggle();
       $(this).removeClass("expander--open")
              .addClass("expander--closed");
-    }
-  },
-
-  togglePrice: function() {
-    const monthlyBasic = "$167";
-    const monthlyPro = "$197";
-    const monthlyPreferred = "$277";
-
-    const annuallyBasic = "$137";
-    const annuallyPro = "$157";
-    const annuallyPreferred = "$227";
-
-    $(".priceToggle").removeClass("priceToggle--active");
-    $(this).addClass("priceToggle--active");
-    let nowShow = $(this).attr("data-this");
-    console.log(nowShow);
-    if (nowShow == "monthly") {
-      $("#basicPrice").html(monthlyBasic);
-      $("#proPrice").html(monthlyPro);
-      $("#preferredPrice").html(monthlyPreferred);
-      $(".annual-disclaimer").hide();
-      $(".monthly-disclaimer").show();
-    }
-    else {
-      $("#basicPrice").html(annuallyBasic);
-      $("#proPrice").html(annuallyPro);
-      $("#preferredPrice").html(annuallyPreferred);
-      $(".annual-disclaimer").show();
-      $(".monthly-disclaimer").hide();
     }
   }
 }
